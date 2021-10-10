1fb1a0e92df1f96ee3580f5999f29ba9ea0ef568
fukawa <ta1fukawa@gmail.com>
2021-10-10 23:56:25 +0900

    Sep a file

--- python/dataloader.py
+++ python/dataloader.py
@@ -1,15 +1,11 @@
-import os
-import multiprocessing
-
 import numpy as np
-import pyworld
-import soundfile as sf
 import torch
 
 def test():
     loader = DataLoader([10], range(50), range(100), batch_size=4)
     print(len(loader))
     print(loader[10])
+    
     pass
 
 class DataLoader(torch.utils.data.Dataset):
@@ -77,23 +73,5 @@ class DataLoader(torch.utils.data.Dataset):
     def _zero_padding(x, target_length):
         y_pad = target_length - len(x)
         return np.pad(x, ((0, y_pad), (0, 0)), mode='constant') if y_pad > 0 else x
-
-def first_setup():
-    pool_obj = multiprocessing.Pool()
-    pool_obj.map(first_setup_sub, range(100))
-
-def first_setup_sub(speaker):
-    for seiren_speaker in [10]:
-        os.makedirs(f'resource/mid/seiren_jvs{seiren_speaker + 1:03d}/jvs{speaker + 1:03d}', exist_ok=True)
-        for speech in range(100):
-            wave, sr = sf.read(f'resource/seiren_jvs{seiren_speaker + 1:03d}/jvs{speaker + 1:03d}/VOICEACTRESS100_{speech + 1:03d}.wav')
-            f0, sp, ap, t = wave_decompose(wave, sr)
-            np.savez_compressed(f'resource/mid/seiren_jvs{seiren_speaker + 1:03d}/jvs{speaker + 1:03d}/VOICEACTRESS100_{speech + 1:03d}.npz', sp=sp)
-
-def wave_decompose(wave, sr):
-    f0, t = pyworld.harvest(wave, sr)
-    sp = pyworld.cheaptrick(wave, f0, t, sr)
-    ap = pyworld.d4c(wave, f0, t, sr)
-    return f0, sp, ap, t
 
 test()
