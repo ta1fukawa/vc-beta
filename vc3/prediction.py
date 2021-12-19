49f943215a6ee4f66f5403e7817f07beb7d6bf3a
fukawa <ta1fukawa@gmail.com>
2021-12-19 22:37:35 +0900

    Update

--- vc3/prediction.py
+++ vc3/prediction.py
@@ -8,7 +8,7 @@ import yaml
 
 import common
 
-def main(source, target, output_dir, encoder_path, model_dir, vocoder_path, config_path):
+def main(source, target, output, encoder_path, model_dir, vocoder_path, config_path):
     device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     encoder = torch.load(encoder_path)          .to(device).eval()
     model   = torch.load(model_dir / 'model.pt').to(device).eval()
@@ -16,7 +16,7 @@ def main(source, target, output_dir, encoder_path, model_dir, vocoder_path, conf
     config  = yaml.load(config_path.read_text(), Loader=yaml.FullLoader)
     model.load_state_dict(torch.load(model_dir / 'weight.pt'))
 
-    output_dir.mkdir(exist_ok=True, parents=True)
+    output.parent.mkdir(exist_ok=True, parents=True)
 
     src_wave, sample_rate = torchaudio.load(source)
     tgt_wave, sample_rate = torchaudio.load(target)
@@ -28,15 +28,15 @@ def main(source, target, output_dir, encoder_path, model_dir, vocoder_path, conf
     src_mel = common.pad_seq(src_mel, config['mel2embed']['seg_len']).unsqueeze(0)
     with torch.no_grad():
         _, _, pred_mel, _ = model(src_mel, src_emb, tgt_emb)
-        pred_wave         = vocoder.generate([pred_mel.squeeze(0)])[0].data.cpu()
+        pred_wave         = vocoder.generate([pred_mel.squeeze(0)])[0].data.unsqueeze(0).cpu()
 
-    torchaudio.save(output_dir / 'output.wav', pred_wave, config['wave2mel']['sample_rate'])
+    torchaudio.save(output, pred_wave, config['wave2mel']['sample_rate'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert wav to mel spectrogram')
     parser.add_argument('source',       type=pathlib.Path, help='path to source wav')
     parser.add_argument('target',       type=pathlib.Path, help='path to target wav')
-    parser.add_argument('output_dir',   type=pathlib.Path, help='path to output directory')
+    parser.add_argument('output',       type=pathlib.Path, help='path to output wav')
     parser.add_argument('encoder_path', type=pathlib.Path, help='path to speaker encoder')
     parser.add_argument('model_dir',    type=pathlib.Path, help='path to model directory')
     parser.add_argument('vocoder_path', type=pathlib.Path, help='path to vocoder path')
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         args = parser.parse_args([
             'autovc/wavs-jvs/jvs001/VOICEACTRESS100_001.wav',
             'autovc/wavs-jvs/jvs002/VOICEACTRESS100_002.wav',
-            'vc3/pred',
+            'vc3/pred/output.wav',
             'autovc2/dvector.pt',
             'vc3/train/2021-12-18/21-44-11',
             'autovc2/vocoder.pt',
